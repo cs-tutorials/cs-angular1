9c3706006c3b3911eea2dc987f248b6ac5346eb3
spiraldancing <de22b22c@opayq.com>
2016-03-08 09:30:48 -0600

    CS tutorial update.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 (function(){
-  var gem = { name: 'Azurite', price: 2.95 };
+  var gem = { 
+  	name: 'Azurite', 
+  	price: 2.95, 
+  	description: 'Description for Azurite.',
+  	canPurchase: false,
+  	isSoldOut: true
+  	 };
+  	 
   var app = angular.module('gemStore', []);
   app.controller('StoreController', function(){
     this.product = gem;
