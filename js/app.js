19c646a31303b6bdbf213f5fb0060256d702c6fa
spiraldancing <de22b22c@opayq.com>
2016-03-08 12:43:22 -0600

    More CS-Angular tutorial updates.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
 (function(){
-  var gem = { 
-  	name: 'Azurite', 
-  	price: 2.95, 
-  	description: 'Description for Azurite.',
-  	canPurchase: true,
-  	isSoldOut: false
-  	 };
-  	 
-  var app = angular.module('gemStore', []);
-  app.controller('StoreController', function(){
-    this.product = gem;
-  });
+	var app = angular.module('gemStore', []);
+
+	app.controller('StoreController', function(){
+		this.products = gems;
+	});
+
+	var gems = [
+		{ 
+			name: 'Azurite', 
+			price: 4.50, 
+			description: 'Description for Azurite.',
+			canPurchase: true,
+			isSoldOut: false
+		},
+		{ 
+			name: 'Dodecahedron', 
+			price: 2.95, 
+			description: 'Description for Dodecahedron.',
+			canPurchase: true,
+			isSoldOut: false
+		},
+		{ 
+			name: 'Pentagonal Gem', 
+			price: 5.95, 
+			description: 'Description for Pentagonal Gem.',
+			canPurchase: false,
+			isSoldOut: false
+		}		
+	];
+
 })();
