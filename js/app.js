3269817c0d26f80154b04615bfe8401b0548fa92
spiraldancing <spiraldancing@ymail.com>
2016-03-10 15:33:23 -0600

    CS Angular tutorial sections < 3.8, adding a review form.

--- js/app.js
+++ js/app.js
@@ -24,6 +24,15 @@
 		};
 	});
 
+	app.controller('ReviewController', function(){
+		this.review = {};
+
+		this.addReview = function(product) {
+			product.reviews.push(this.review);
+			this.review = {};
+		}
+	})
+
 	var csGems = 
 	[
 		{
