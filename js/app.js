8e973599a2c6901d8aeaf4a80bf106cf892e447d
spiraldancing <de22b22c@opayq.com>
2016-03-15 15:01:07 -0500

    CS Angular tutorial section 4.6-4.7, directives for everything.

--- js/app.js
+++ js/app.js
@@ -5,27 +5,17 @@
 		this.products = csGems;
 	});
 
-	app.controller('GalleryController', function(){
-		this.current = 0;
-		this.setCurrent = function(newVal){
-			this.current = newVal || 0;
-		};
-	});
-
-	app.controller('ReviewController', function(){
-		this.review = {};
-
-		this.addReview = function(product) {
-			this.review.createdOn = Date.now();
-			product.reviews.push(this.review);
-			this.review = {};
-		}
-	})
-
-	app.directive('productTitle', function(){
+	app.directive("productGallery", function() {
 		return {
 			restrict: 'E',
-			templateUrl: 'product-title.html'
+			templateUrl: 'product-gallery.html',
+			controller: function() {
+				this.current = 0;
+				this.setCurrent = function(newVal){
+					this.current = newVal || 0;
+				};
+			},
+			controllerAs: 'gallery'
 		};
 	});
 
@@ -45,6 +35,44 @@
 				};
 			},
 			controllerAs: 'tabCtrl'
+		};
+	});
+
+	app.directive('productTitle', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-title.html'
+		};
+	});
+
+	app.directive('productDescription', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-description.html'
+		};
+	});
+
+	app.directive('productSpecs', function(){
+		return {
+			restrict: 'A',
+			templateUrl: 'product-specs.html'
+		};
+	});
+
+	app.directive('productReviews', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-reviews.html',
+			controller: function() {
+				this.review = {};
+
+				this.addReview = function(product) {
+					this.review.createdOn = Date.now();
+					product.reviews.push(this.review);
+					this.review = {};
+				}
+			},
+			controllerAs: 'reviewCtrl'
 		};
 	});
 	
