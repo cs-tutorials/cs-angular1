b94a13a75e36722297311ae54829bbbb34ff0a0f
spiraldancing <de22b22c@opayq.com>
2016-03-15 14:22:24 -0500

    CS Angular tutorial section 4.2-4.6, tweaks.

--- js/app.js
+++ js/app.js
@@ -5,18 +5,6 @@
 		this.products = csGems;
 	});
 
-//	app.controller('TabController', function(){
-//		this.tab = 1;
-//
-//		this.setTab = function(newValue){
-//			this.tab = newValue;
-//		};
-//
-//		this.isSet = function(tabName){
-//			return this.tab === tabName;
-//		};
-//	});
-
 	app.controller('GalleryController', function(){
 		this.current = 0;
 		this.setCurrent = function(newVal){
